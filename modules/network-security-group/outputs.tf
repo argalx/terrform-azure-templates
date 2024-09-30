c833fe16644adcc6fece92e8017ffd54a710bf0e
Argal <arvin.bonggal@gmail.com>
2024-09-30 12:50:41 +0800

    Add NSG Name in output value

--- modules/network-security-group/outputs.tf
+++ modules/network-security-group/outputs.tf
@@ -2,3 +2,8 @@ output "nsg-output-id" {
     description = "NSG ID output value."
     value = azurerm_network_security_group.nsg.id
 }
+
+output "nsg-output-name" {
+    description = "NSG Name output value."
+    value = azurerm_network_security_group.nsg.name
+}
