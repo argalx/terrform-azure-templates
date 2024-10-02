0e5953947abaf26bdba7554367525d10ae9e3e7e
Argal <arvin.bonggal@gmail.com>
2024-10-02 16:51:50 +0800

    Add dynamic security rule properties

--- modules/network-security-group/main.tf
+++ modules/network-security-group/main.tf
@@ -3,19 +3,21 @@ resource "azurerm_network_security_group" "nsg" {
     resource_group_name = var.nsg-rg-name
     location = var.nsg-rg-location
 
-    # Inline Network Security Rule
-
-    # security_rule = {
-    #     name = "test123"
-    #     priority = 100
-    #     direction = "Inbound"
-    #     access = "Allow"
-    #     protocol = "Tcp"
-    #     source_port_range = "*"
-    #     destination_port_range = "*"
-    #     source_address_prefix = "*"
-    #     destination_address_prefix = "*"
-    # }
+    # Security Rule
+    dynamic "security_rule" {
+        for_each = var.nsg-security-rule
+        content {
+            name = nat-rule.value.name
+            priority = nat-rule.value.priority
+            direction = nat-rule.value.direction
+            access = nat-rule.value.access
+            protocol = nat-rule.value.protocol
+            source_port_ranges = nat-rule.value.source_port_ranges
+            destination_port_ranges = nat-rule.value.destination_port_ranges
+            source_address_prefixes = nat-rule.value.source_address_prefixes
+            destination_address_prefixes = nat-rule.value.destination_address_prefixes
+        }
+    }
 
     tags = {
     }
--- modules/network-security-group/variables.tf
+++ modules/network-security-group/variables.tf
@@ -12,3 +12,18 @@ variable "nsg-rg-location" {
     description = "NSG resource group location."
     type = string
 }
+
+variable "nsg-security-rule" {
+    description = "A list of security rules for the NSG"
+    type = list(object({
+        name = string
+        priority = number
+        direction = string
+        access = string
+        protocol = string
+        source_port_ranges = list(string)
+        destination_port_ranges = list(string)
+        source_address_prefixes = list(string)
+        destination_address_prefixes = list(string)
+    }))
+}
